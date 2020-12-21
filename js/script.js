6afdab734ae69fc3db7f0279c24f18222c3023bf
Sara <saragorleta@yahoo.com>
2020-12-21 16:13:20 +0100

    aggiunta prompt pari e dispari

--- js/script.js
+++ js/script.js
@@ -6,13 +6,16 @@
 // Dichiariamo chi ha vinto.
 
 
+var sceltaUtente= prompt('scegli pari o dispari');
+var pari=true;
+var dispari=false;
 var numeroUtente=parseInt(prompt('inserisci un numero da 1 a 5'));//utente inserisce un numero da 1 a 5
 var numeroComputer=numeroRandom (1, 5);//il computer genera un numero da 1 a 5
 var somma= numeroUtente+numeroComputer;// creo una var somma
 //ho sommato i due numeri
 console.log(numeroUtente+numeroComputer);
 
-if(verificaPari(somma)){
+if(verificaPari(somma==pari)){
   console.log('la somma risulta essere un numero pari');
 }
 else{
@@ -22,6 +25,7 @@ else{
 //ho creato la funzione per generare  numeri random
 function numeroRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 //ho creato una funzione per verificare se la somma dei numeri è pari o dispari
 function verificaPari(somma){
   if(somma % 2 == 0){
@@ -29,9 +33,4 @@ function verificaPari(somma){
 }
   return false;
 }
-
-
-
-    console.log('il numero sommato è pari');
-  }
-}
+console.log('il numero sommato è pari');
